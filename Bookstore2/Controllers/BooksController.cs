227ec156737b237a7d488a927d456f5c3960c01c
Your Name <your-email@example.com>
2025-03-31 19:55:39 -0600

    Add API endpoints for retrieving books and categories

--- Bookstore2/Controllers/BooksController.cs
+++ Bookstore2/Controllers/BooksController.cs
@@ -16,10 +16,31 @@ namespace Bookstore2.Controllers
             _context = context;
         }
 
+        // GET: api/books
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
         {
             return await _context.Books.ToListAsync();
+        }
+
+        // GET: api/books/categories
+        [HttpGet("categories")]
+        public async Task<ActionResult<IEnumerable<string>>> GetCategories()
+        {
+            return await _context.Books
+                .Select(b => b.Classification)
+                .Distinct()
+                .OrderBy(c => c)
+                .ToListAsync();
+        }
+
+        // GET: api/books/category/{category}
+        [HttpGet("category/{category}")]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooksByCategory(string category)
+        {
+            return await _context.Books
+                .Where(b => b.Classification == category)
+                .ToListAsync();
         }
     }
 }
--- Bookstore2/obj/Debug/net9.0/Bookstore2.AssemblyInfo.cs
+++ Bookstore2/obj/Debug/net9.0/Bookstore2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4bea7a4922140f412fa764ce6da9b047bb612f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
